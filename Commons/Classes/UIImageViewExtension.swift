a43f74749c7e35841648802be3c66e43f7963216
lucas leitao <lucas.leitao25@gmail.com>
2021-01-28 11:05:16 -0300

    add public

--- Commons/Classes/UIImageViewExtension.swift
+++ Commons/Classes/UIImageViewExtension.swift
@@ -11,7 +11,7 @@ import AlamofireImage
 
 extension UIImageView {
 
-    func carregarImagem(idIcon: String) {
+    public func carregarImagem(idIcon: String) {
         let url = "https://s3.eu-central-1.amazonaws.com/bbxt-static-icons/type-id/png_64/\(idIcon).png"
         guard let imageurl = URL(string: url) else {return}
         self.af_setImage(withURL: imageurl)
